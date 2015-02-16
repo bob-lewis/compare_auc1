b443f824c29562d5bc68671141bf06ba7b048257
bob <dublin.bob@gmail.com>
2015-02-16 13:16:56 +0000

    added files

---
+++ server_list.py
@@ -0,0 +1,3 @@
+__author__ = 'bob'
+# Ahn'Qir,Al'Akir,Alonsus,Anachro,Arathor,Argent%20Dawn ,Aszune,Auchind,Azjol-Najub,Azuremy,Balnazz,Blade's,Bladefist
+valid_realms = ['kilrogg', 'darksorrow', 'mazrigos', 'frostwhisper', 'Aerie%20Peak', 'Agamagg', 'Aggrama']
